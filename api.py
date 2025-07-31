d70b0febdff9cdce71cee38c1be7f9fbaea730c8
Ruben Nunes <84018198+Ruben171@users.noreply.github.com>
2025-07-31 16:07:38 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,1 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def raiz():
+  return {"mensagem": "Olá, Ruben! A api está a funcionar"}
+
+@app.get("/sobre")
+def sobre():
+  return {
+      "nome": "Ruben",
+      "idade": 26
+  }
+
+@app.get("/contacto")
+def contacto():
+  return {
+      "email": "ruben@rmail.com",
+      "telefone": "912345678"
+  }
 
